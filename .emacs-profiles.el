7075f5616b34681b118c0f3fd0df06c092339330
sondreklyve <sondre.klyve@hotmail.no>
2023-03-18 13:58:29 +0100

    Remove spacemacs, custom emacs and chemacs

--- .emacs-profiles.el
+++
@@ -1,4 +0,0 @@
-(("default" . ((user-emacs-directory . "~/.emacs.custom")))
-("doom" . ((user-emacs-directory . "~/.emacs.doom")
-           (server-name . "doom")))
-("spacemacs" . ((user-emacs-directory . "~/.emacs.spacemacs"))))
